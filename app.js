ef4e2874629ae4cbc213e76b29292e79481ca9b0
TeonaTsiramua <tsiramuateona@gmail.com>
2024-02-20 21:51:18 +0400

    First Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import productRoutes from './api/routes/products.js';
+import orderRoutes from './api/routes/orders.js';
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://tsiramuateona:nooneknows1@node-rest.o3sch9x.mongodb.net/'
+);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control_Allow_Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Routes
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+export default app;
