c0d73cb41c9d77119910df07b3d0a156fb0eed2a
TeonaTsiramua <tsiramuateona@gmail.com>
2024-02-21 15:10:50 +0400

    remove comment

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -33,44 +33,6 @@ router.get('/', (req, res, next) => {
     });
 });
 
-// router.post('/', (req, res, next) => {
-//   Product.findById(req.body.productId)
-//     .then((product) => {
-//       if (!product) {
-//         return res.status(404).json({
-//           message: 'Product not found',
-//         });
-//       }
-//       const order = new Order({
-//         _id: new mongoose.Types.ObjectId(),
-//         quantity: req.body.quantity,
-//         product: req.body.productId,
-//       });
-//       return order.save();
-//     })
-//     .then((result) => {
-//       console.log(result);
-//       res.status(201).json({
-//         message: 'Order stored',
-//         createdOrder: {
-//           _id: result._id,
-//           product: result.product,
-//           quantity: result.quantity,
-//         },
-//         request: {
-//           type: 'GET',
-//           url: 'http://localhost:300/orders/' + result._id,
-//         },
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({
-//         error: err,
-//       });
-//     });
-// });
-
 router.post('/', (req, res, next) => {
   Product.findById(req.body.productId)
     .then((product) => {
